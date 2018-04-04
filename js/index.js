a0cce688876e592a3f8f4f39ad0936afd35e203e
munenelewis <munene2379@gmail.com>
2018-04-04 09:43:03 +0400

    added a contact form

--- js/index.js
+++ js/index.js
@@ -16,3 +16,35 @@ $(function() {
         $(".content").fadeIn(1000);
     });
 });
+
+$(document).ready(function() {
+    $("#slider-range-min").slider({
+        range: "min",
+        step: 100,
+        value: 1200,
+        min: 0,
+        max: 15000,
+        slide: function(event, ui) {
+            $("#amount").val("sh" + ui.value);
+        },
+        change: function(event, ui) {
+            if (ui.value === 0) {
+                $("#amount").val("To be determined");
+            } else if (ui.value === 15000) {
+                $("#amount").val("sh" + "15000 or more");
+            } else {
+                $("#amount").val("sh" + $("#slider-range-min").slider("value"));
+            }
+        }
+    });
+    $("#amount").val("sh" + $("#slider-range-min").slider("value"));
+
+});
+
+$(document).ready(function() {
+
+    // -- autosize init --
+
+    autosize($('textarea'));
+
+});
